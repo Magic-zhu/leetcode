fec11882d62d9d73666bd8998b50303e2fc9a932
magic-zhu <18817313320@163.com>
2020-04-19 17:07:25 +0800

    更新

---
+++ base/001.js
@@ -0,0 +1,10 @@
+var singleNumber = function(nums) {
+    let r= 0;
+    for (let i=0,l =nums.length;i<l;i++){
+        r = r^nums[i]
+        console.log(r)
+    }
+    return r
+};
+
+singleNumber([2,1,3,1,3])
